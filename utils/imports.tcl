781f3ab839c124a565f8ff25fdbd7eb03b7ddc71
Altberg <shmuelbisberg@gmail.com>
2024-05-12 16:07:50 +0300

    imports for utility files and packages

---
+++ utils/imports.tcl
@@ -0,0 +1,9 @@
+# This file contains the imports for all utility files, packages and modules
+
+# packages
+package require xml 3.2
+
+# modules
+
+#  utilities
+source "[file normalize .]/utils/files.tcl"
