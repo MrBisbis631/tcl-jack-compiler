b529c42408694bee56c287fba277ae2a5955741f
Altberg <shmuelbisberg@gmail.com>
2024-05-12 16:33:19 +0300

    create directory if not existing

--- utils/files.tcl
+++ utils/files.tcl
@@ -1,8 +1,13 @@
 # Utility functions for files IO
 
 # Get the target directory - relative to the root directory
-proc get_target_dir {dir} {
-  return "[file normalize .]/target/$dir"
+# If the directory does not exist, create it
+proc get_target_dir {file_path} {
+  set dir target/[string range [file tail $file_path] 0 [expr {[string last "." [file tail $file_path]] - 1}]]
+  if {![file isdirectory $dir]} {
+    file mkdir $dir
+  }
+  return "[file normalize .]/target/$file_path"
 }
 
 # Get the files nanme in a directory acording to specific extension
