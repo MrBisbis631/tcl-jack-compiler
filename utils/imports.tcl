102bdae72abf70633c1d10fc850b85632e08bae6
Altberg <shmuelbisberg@gmail.com>
2024-05-17 15:05:41 +0300

    update imports

--- utils/imports.tcl
+++ utils/imports.tcl
@@ -4,6 +4,8 @@
 package require xml 3.2
 
 # modules
+source "[file normalize .]/vm-2-hack/vm-2-hack.tcl"
 
 #  utilities
 source "[file normalize .]/utils/files.tcl"
+source "[file normalize .]/utils/helpers.tcl"
