0761b497b593f04a85976b1c5fefb20b6c4ac698
Altberg <shmuelbisberg@gmail.com>
2024-05-17 15:06:05 +0300

    Add test-vm-2-hack-stack.tcl for exercise 1, with data and cmd

--- Makefile
+++ Makefile
@@ -4,5 +4,12 @@ run:
 xml-example:
 	tclsh ./tcl-examples/tclxml.tcl
 
+test-vm-2-hack-stack:
+	tclsh ./tcl-examples/test-vm-2-hack-stack.tcl
+
+# Exeersises
+
 ex0:
 	tclsh ./tcl-examples/ex0.tcl
+
+ex1: test-vm-2-hack-stack
---
+++ tcl-examples/test-vm-2-hack-stack.tcl
@@ -0,0 +1,18 @@
+# Testing translation of VM code to Hack assembly code - Stack operations
+
+source "[file normalize .]/utils/imports.tcl"
+
+set vm_files [get_files_name_by_extention "data/ex1" "vm"]
+set output_dir [set_up_target_dir "ex1"]
+
+foreach file $vm_files {
+  set o_fd [open "$output_dir/[filename_no_extention $file].asm" "w"]
+  try {
+    for {set line [coroutine line_generator generate_lines $file]} {$line != "\0"} {set line [line_generator]} {
+      puts $o_fd "// file: $file, cmd: $line"
+      puts $o_fd [vm_to_hack $line]
+    }
+  } finally {
+    close $o_fd
+  }
+}
