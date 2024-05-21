829820601230983147cb1233dcc2fa889133f80c
Altberg <shmuelbisberg@gmail.com>
2024-05-21 18:13:19 +0300

    remove file from label name

--- vm-2-hack/flow-control.tcl
+++ vm-2-hack/flow-control.tcl
@@ -1,21 +1,21 @@
 # Translation VM Flow control commands to HACK assembly
 
-# Generates a label in the format "(file_name.label)".
-proc hack_label {label file_name} {
-  return "($file_name.$label)"
+# Generates a label in the format "(label)".
+proc hack_label {label} {
+  return "($label)\n"
 }
 
-# Generates a unique label in the format "(file_name.label)".
-proc hack_uniq_label {label file_name} {
-  return "($file_name.[uniq_label $label])"
+# Generates a unique label in the format "(label)".
+proc hack_uniq_label {label} {
+  return "([uniq_label $label])\n"
 }
 
 # Generates an assembly jump command to a specified label.
-proc hack_goto {label file_name} {
-  return "@$file_name.$label\n0;JMP\n"
+proc hack_goto {label} {
+  return "@$label\n0;JMP\n"
 }
 
 # Generates a conditional jump command, pops the stack, and jumps if non-zero.
-proc hack_if_goto {label file_name} {
-  return "@SP\nM=M-1\nA=M\nD=M\n@$file_name.$label\nD; JNE\n"
+proc hack_if_goto {label} {
+  return "@SP\nM=M-1\nA=M\nD=M\n@$label\nD; JNE\n"
 }
