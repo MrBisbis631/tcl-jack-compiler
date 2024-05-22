656ea7135bb981cb0feaacf954787de277dfdbf8
Altberg <shmuelbisberg@gmail.com>
2024-05-22 18:11:21 +0300

    add filename to .asm and ignore comments and whitespace

--- tcl-examples/test-vm-2-hack-stack.tcl
+++ tcl-examples/test-vm-2-hack-stack.tcl
@@ -8,8 +8,14 @@ set output_dir [set_up_target_dir "ex1"]
 foreach file $vm_files {
   set o_fd [open "$output_dir/[filename_no_extention $file].asm" "w"]
   try {
+    puts $o_fd "// TRANSPILED FILE: $file"
     for {set line [coroutine line_generator generate_lines $file]} {$line != "\0"} {set line [line_generator]} {
-      puts $o_fd "// file: $file, cmd: $line"
+      # skip empty lines and comments
+      if {[string is space $line] || [string match "//*" $line]} {
+        continue
+      }
+
+      puts $o_fd "// CMD: $line"
       puts $o_fd [vm_to_hack $line]
     }
   } finally {
