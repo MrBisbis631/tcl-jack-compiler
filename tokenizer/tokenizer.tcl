4502df41c8e21ebda9c1779aa2d8444c58fb7e71
Altberg <shmuelbisberg@gmail.com>
2024-06-06 21:13:49 +0300

    create `tokenize` that convert jack code to tokens. add `tokenize_generator` for the generator version

---
+++ tokenizer/tokenizer.tcl
@@ -0,0 +1,56 @@
+# Tokenizer for Jack
+
+source "[file normalize .]/tokenizer/tokens.tcl"
+
+# General pattern for Jack tokens, including keywords, symbols, integers, and strings
+set jack_pattern {("[^"]*"|[\d]+|[\w]+|[{}()\[\];.,+\-*/&|<>=~])}
+
+# gets jack code and returns a list of tokens
+proc tokenize {code} {
+  global jack_pattern
+
+  set tokens {}
+  set pos 0
+
+  while {[regexp -indices -start $pos $jack_pattern $code match]} {
+    # extract token with the general pattren
+    set start [lindex $match 0]
+    set end [lindex $match 1]
+    set token_str [string range $code $start $end]
+
+    # add token as asositive array with `type` and `value` keys
+    # array set token {type [get_token_type $token_str] value [get_token_value $token_str]}
+    set type [get_token_type $token_str]
+    set value [get_token_value $token_str]
+
+    set token [dict create type $type value $value]
+    lappend tokens $token
+
+    set pos [expr {$end + 1}]
+  }
+
+  return $tokens
+}
+
+# gets jack code and returns a generator of tokens
+proc tokenize_generator {code} {
+  global jack_pattern
+
+  set pos 0
+
+  while {[regexp -indices -start $pos $jack_pattern $code match]} {
+    # extract token with the general pattren
+    set start [lindex $match 0]
+    set end [lindex $match 1]
+    set token [string range $code $start $end]
+
+    # add token as asositive array with `type` and `value` keys
+    set token [dict create type [get_token_type $token] value [get_token_value $token]]
+
+    yield $token
+    set pos [expr {$end + 1}]
+  }
+
+  # yield unpresentable character to indicate the end of the tokens
+  yield "\0"
+}
