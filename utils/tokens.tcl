1410e9438db61d3f4f8d306c2eabdb21722d6deb
Altberg <shmuelbisberg@gmail.com>
2024-06-10 19:43:20 +0300

    add utilities for token assertion and proses

---
+++ utils/tokens.tcl
@@ -0,0 +1,66 @@
+# Utoilities for streaming tokens from a corutine
+
+# `__tokens_generator` is the corutine that generates tokens
+set __tokens_generator {}
+# `__current_token` is the current token
+set __current_token {}
+
+# initialize the tokens generator
+proc init_tokens_generator {tokens_corutine_name} {
+  global __tokens_generator
+  set __tokens_generator $tokens_corutine_name
+}
+
+# get the current token
+proc get_current_token {} {
+  global __current_token
+  return $__current_token
+}
+
+# get the current token value
+proc get_current_token_value {} {
+  set token [get_current_token]
+  set value [dict get $token value]
+  return $value
+}
+
+# get the next token value
+proc get_next_token_value {} {
+  set token [get_next_token]
+  if {__current_token == "\0"} {
+    return ""
+  }
+  set value [dict get $token vaule]
+  return $value
+}
+
+# advance to the next token and return it
+proc get_next_token {} {
+  global __tokens_generator __current_token
+  set __current_token [$__tokens_generator]
+  return $__current_token
+}
+
+# check if the current token is an operator
+proc is_op {value} {
+  return [regexp {^[+\-*/&|<>]=$} $value]
+}
+
+# check if the current token is a unary operator
+proc is_unary_op {value} {
+  return [regexp {^[-!]$} $value]
+}
+
+proc is_expression {value} {
+  set number_regex {^[0-9]+$}
+  set string_regex {^".*"$}
+  set keyword_regex {^(true|false|null|this)$}
+  set variable_regex {^[a-zA-Z_][a-zA-Z0-9_]*$}
+  set unary_op_regex {^[-~]$}
+  set open_parenthesis_regex {^\($}
+  return [regexp "$number_regex|$string_regex|$keyword_regex|$variable_regex|$unary_op_regex|$open_parenthesis_regex" $value]
+}
+
+proc is_identifier {value} {
+  return [regexp {^[a-zA-Z_][a-zA-Z0-9_]*$} $value]
+}
