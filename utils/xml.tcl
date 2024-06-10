af667cfe9a7afbbda716be9e955ca7e0371a3361
Altberg <shmuelbisberg@gmail.com>
2024-06-10 19:36:11 +0300

    add create_xml_leaf, create_xml_node, xml_to_tokens_generator utilities

--- utils/xml.tcl
+++ utils/xml.tcl
@@ -12,3 +12,27 @@ proc token_to_xml_node {token parent} {
   # insert text node with token value
   ::dom::document createTextNode $node [dict get $token value]
 }
+
+# Create an XML node with a given name and value
+proc create_xml_leaf {parent name value} {
+  set node [::dom::document createElement $parent $name]
+  ::dom::document createTextNode $node $value
+  return $node
+}
+
+# Create an XML node with a given name
+proc create_xml_node {parent name} {
+  set node [::dom::document createElement $parent $name]
+  return $node
+}
+
+# Return the tokens of an XML document
+proc xml_to_tokens_generator {doc} {
+  foreach token [::dom::selectNode $doc /tokens/*] {
+    set value [string trim [$token stringValue]]
+    set type [$token cget -nodeName]
+    yield [dict create type $type value $value]
+  }
+
+  yield "\0"
+}
