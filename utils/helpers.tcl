95935046f42377c7192a87e4e1e9dd95b352fc4c
Altberg <shmuelbisberg@gmail.com>
2024-06-10 19:44:10 +0300

    utility for updating analyzed tokens with new leaf

--- utils/helpers.tcl
+++ utils/helpers.tcl
@@ -17,3 +17,12 @@ proc pipe_coroutine_generator {command generator} {
   set gen_res [$generator]
   yield [$command $gen_res]
 }
+
+# insert a leaf into the xml persed tree
+proc prossess_terminal {parent type value} {
+  if {[get_current_token_value] != $value} {
+    error "PARSER ERROR: expected $value but got [get_current_token_value]"
+  }
+  create_xml_leaf $parent $type $value
+  get_next_token
+}
