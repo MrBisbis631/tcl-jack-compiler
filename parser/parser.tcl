8d99bb3207d245c13b147432752f37b667491a36
Altberg <shmuelbisberg@gmail.com>
2024-06-13 20:21:23 +0300

    chore: Refactor parse function to accept a document parameter

--- parser/parser.tcl
+++ parser/parser.tcl
@@ -6,8 +6,7 @@ source "[file normalize .]/parser/expressions.tcl"
 
 # parse a .jack file, entry point is `class`, which is the root of the parse tree
 # returns a parse tree in the form of a DOM tree
-proc parse {} {
-  set parsed_doc [::dom::DOMImplementation create]
-  set parsed_tree [class_parser $parsed_doc]
+proc parse {doc} {
+  set parsed_tree [class_parser $doc]
   return $parsed_tree
 }
--- tcl-examples/parser-example.tcl
+++ tcl-examples/parser-example.tcl
@@ -1,29 +1,28 @@
-# Test Jack parser
+# Test kokenizer - creates <file>T.xml with tokens from <file>.jack
+# using a thread pool
 
 source "[file normalize .]/utils/imports.tcl"
 
+set workers 2
+
 # Exit if selected directory is missing
 if {[llength $argv] == 0} {
-  puts "Usage: tclsh [file tail [info script]] <vm_file_name>"
+  puts "Usage: tclsh [file tail [info script]] <project_dir>"
   exit 1
 }
 
-set token_xml_files [get_files_name_by_postfix "data/ex4/[lindex $argv 0]" "T.xml"]
+set token_files [get_files_name_by_postfix "data/ex4/[lindex $argv 0]" "T.xml"]
 set output_dir [set_up_target_dir "ex4.2/[lindex $argv 0]"]
 
-# convert each <name>T.xml file to <name>.xml file
-foreach token_xml_file $token_xml_files {
-  # convert token file to xml tree
-  set tokens_doc [xml_file_to_dom_doc $token_xml_file]
+# init a thread pool thith thread ids and send the `parse_token_xml_file_script` to each worker
+set pool [create_thread_pool $workers $parse_token_xml_file_script]
+# init a corutine that returns the workers in round-robin
+coroutine round_robin_coroutine thread_round_robin_generator $pool
 
-  # create a coroutine to generate tokens from the XML document
-  coroutine tokens_generator xml_to_tokens_generator $tokens_doc
-  # initialize the tokens generator - usesed in parers
-  init_tokens_generator tokens_generator
-
-  # parse file to doc
-  set parsed_doc [parse]
-
-  # write to output file
-  write_dom_doc_to_file $parsed_doc "$output_dir/[filename_no_extention $token_xml_file].xml"
+# send each file to a worker
+foreach token_file $token_files {
+  thread::send -async [round_robin_coroutine] [list parse_token_xml_file $token_file $output_dir]
 }
+
+# wait for all workers to finish
+wait_for_pool_to_finish $pool
