bc740cd73bdd9fb3d8d3965c2e0b0772c7908166
Altberg <shmuelbisberg@gmail.com>
2024-07-10 15:08:17 +0300

    fix  symble table

--- utils/symble-table.tcl
+++ utils/symble-table.tcl
@@ -1,76 +1,55 @@
 # Symbol Table Utils for scope management in Jack Compiler
 
-set __symble_table_doc [dom createDocument scops]
-set scops [$__symble_table_doc documentElement]
+set __symble_table_doc [::dom::DOMImplementation create]
+set scops [::dom::document createElement $__symble_table_doc scops]
 
 # Create a scope
 proc create_scope {name} {
-  global scops __symble_table_doc
-  set scope [$__symble_table_doc createElement $name]
-  $scope setAttribute static_count 0
-  $scope setAttribute field_count 0
-  $scope setAttribute argument_count 0
-  $scope setAttribute local_count 0
-  $scope setAttribute var_count 0
-  $scops appendChild $scope
+  global scops
+  set scope [::dom::document createElement $scops $name]
+  ::dom::element setAttribute $scope static_count 0
+  ::dom::element setAttribute $scope field_count 0
+  ::dom::element setAttribute $scope argument_count 0
+  ::dom::element setAttribute $scope local_count 0
+  ::dom::element setAttribute $scope var_count 0
   return $scope
-}
-
-# get scope node
-proc get_scope_records {name} {
-  global __symble_table_doc
-  set scope [$__symble_table_doc selectNodes /scops/$name]
-
-  set records_list {}
-
-  foreach record [$scope selectNodes *] {
-    set type [$record getAttribute type]
-    set kind [$record getAttribute kind]
-    set index [$record getAttribute index]
-
-    lappend records_list [list $type $kind $index]
-  }
-
-  return $records_list
 }
 
 # Create a record in a scope
 proc create_record {scope_name name type kind} {
-  global __symble_table_doc
-  set scope [$__symble_table_doc selectNodes /scops/$scope_name]
-  set record [$__symble_table_doc createElement $name]
+  global scops __symble_table_doc
+
+  set scope [::dom::selectNode $__symble_table_doc /scops/$scope_name]
+  set record [::dom::document createElement $scope $name]
 
   # get count set the index  on record and increment it on scope
   set count 0
   switch $kind {
     "static" {
-      set count [$scope getAttribute static_count]
-      $scope setAttribute static_count [expr $count + 1]
+      set count [::dom::element getAttribute $scope static_count]
+      ::dom::element setAttribute $scope static_count [expr $count + 1]
     }
     "field" {
-      set count [$scope getAttribute field_count]
-      $scope setAttribute field_count [expr $count + 1]
+      set count [::dom::element getAttribute $scope field_count]
+      ::dom::element setAttribute $scope field_count [expr $count + 1]
     }
     "argument" {
-      set count [$scope getAttribute argument_count]
-      $scope setAttribute argument_count [expr $count + 1]
+      set count [::dom::element getAttribute $scope argument_count]
+      ::dom::element setAttribute $scope argument_count [expr $count + 1]
     }
     "local" {
-      set count [$scope getAttribute local_count]
-      $scope setAttribute local_count [expr $count + 1]
+      set count [::dom::element getAttribute $scope local_count]
+      ::dom::element setAttribute $scope local_count [expr $count + 1]
     }
     "var" {
-      set count [$scope getAttribute var_count]
-      $scope setAttribute var_count [expr $count + 1]
+      set count [::dom::element getAttribute $scope var_count]
+      ::dom::element setAttribute $scope var_count [expr $count + 1]
     }
   }
   # set attributes
-  $record setAttribute index $count
-  $record setAttribute type $type
-  $record setAttribute kind $kind
-
-  # append record to scope
-  $scope appendChild $record
+  ::dom::element setAttribute $record index $count
+  ::dom::element setAttribute $record type $type
+  ::dom::element setAttribute $record kind $kind
 
   return $record
 }
@@ -79,11 +58,12 @@ proc create_record {scope_name name type kind} {
 proc get_record {scope_name name} {
   global __symble_table_doc
   # fetch the record from the scope
-  set record [$__symble_table_doc selectNodes /scops/$scope_name/$name]
+  set record [::dom::selectNode $__symble_table_doc /scops/$scope_name/$name]
+
   # gets the attributes of the record
-  set type [$record getAttribute type]
-  set kind [$record getAttribute kind]
-  set index [$record getAttribute index]
+  set type [::dom::element getAttribute $record type]
+  set kind [::dom::element getAttribute $record kind]
+  set index [::dom::element getAttribute $record index]
 
   return [list $type $kind $index]
 }
@@ -93,6 +73,6 @@ proc get_record {scope_name name} {
 proc dump_symble_table {} {
   global __symble_table_doc
   puts "---- symble Table -----"
-  puts [$__symble_table_doc asXML]
+  puts [::dom::DOMImplementation serialize $__symble_table_doc -indent 1]
   puts "--- end ymble Table ---"
 }
