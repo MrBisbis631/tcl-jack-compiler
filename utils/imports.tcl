e49649633dac66084b8d32569222c8f380cf56eb
Altberg <shmuelbisberg@gmail.com>
2024-07-10 14:26:41 +0300

    remove tdom

--- utils/imports.tcl
+++ utils/imports.tcl
@@ -3,14 +3,15 @@
 # packages
 package require xml 3.2
 package require Thread 2.8
-package require tdom
+# ! confilct with the xml package
+# package require tdom
 package require Ttrace
 
 # modules
 source "[file normalize .]/vm-2-hack/vm-2-hack.tcl"
 source "[file normalize .]/tokenizer/tokenizer.tcl"
 source "[file normalize .]/parser/parser.tcl"
-source "[file normalize .]/jack-2-vm/jack-2-vm.tcl"
+# source "[file normalize .]/jack-2-vm/jack-2-vm.tcl"
 
 #  utilities
 source "[file normalize .]/utils/files.tcl"
@@ -19,4 +20,4 @@ source "[file normalize .]/utils/scripts.tcl"
 source "[file normalize .]/utils/thread-pool.tcl"
 source "[file normalize .]/utils/xml.tcl"
 source "[file normalize .]/utils/tokens.tcl"
-source "[file normalize .]/utils/symble-table.tcl"
+# source "[file normalize .]/utils/symble-table.tcl"
